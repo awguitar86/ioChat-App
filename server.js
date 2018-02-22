502f8bd4a13e7b903ccbd83d68d79a3fe86dc8f5
Austin Wright <awguitar86@gmail.com>
2018-02-22 10:59:57 -0700

    added username to showup next to message

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ io.sockets.on('connection', (socket) => {
 
     //Disconnect
     socket.on('disconnect', (data) => {
+        // if(!socket.username) return;
+        users.splice(users.indexOf(socket.username), 1 );
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected', connections.length);
     });
@@ -26,7 +29,19 @@ io.sockets.on('connection', (socket) => {
     //Send Message
     socket.on('send message', (data) => {
         console.log(data);
-        io.sockets.emit('new message', {msg: data});
+        io.sockets.emit('new message', { msg: data, user: socket.username });
     });
+
+    // New User
+    socket.on('new user', (data, callback) => {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    var updateUsernames = () => {
+        io.sockets.emit('get users', users);
+    }
 
 })
